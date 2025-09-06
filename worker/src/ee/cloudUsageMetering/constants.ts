26086408b97c23b2d05f7c7ed5ab6f4c8312d834
GlutenWebapp <chouweimin@berkeley.edu>
2025-09-06 16:25:21 -0400

    Remove ee folders
    - These folders are not allowed by the license of langfuse

--- worker/src/ee/cloudUsageMetering/constants.ts
+++
@@ -1,8 +0,0 @@
-export const cloudUsageMeteringDbCronJobName = "cloud_usage_metering";
-
-export enum CloudUsageMeteringDbCronJobStates {
-  // eslint-disable-next-line no-unused-vars
-  Queued = "queued",
-  // eslint-disable-next-line no-unused-vars
-  Processing = "processing",
-}
